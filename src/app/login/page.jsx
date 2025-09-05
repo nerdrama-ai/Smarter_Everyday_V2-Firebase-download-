1ab1c9c1598b0397920596264a3d7cc40e487190
nerdrama.ai <nerdrama.ai@gmail.com>
2025-09-05 10:33:18 +0530

    Update and rename page.tsx to page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,5 @@
-import { AuthForm } from '@/components/auth/AuthForm';
-import { Logo } from '@/components/Logo';
+import { AuthForm } from '@/components/auth/AuthForm'
+import { Logo } from '@/components/Logo'
 
 export default function LoginPage() {
   return (
@@ -7,7 +7,7 @@ export default function LoginPage() {
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <div className="flex justify-center mb-4">
-              <Logo />
+            <Logo />
           </div>
           <h1 className="text-2xl font-semibold tracking-tight font-headline">
             Welcome to The Quizway
@@ -19,5 +19,5 @@ export default function LoginPage() {
         <AuthForm />
       </div>
     </div>
-  );
+  )
 }
